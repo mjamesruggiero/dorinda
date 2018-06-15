e1702c4a683f076d5ac39c4e4f443a74937e8ec3
mjamesruggiero <mjamesruggiero@gmail.com>
2018-06-15 14:34:59 -0700

    Startup message is just better

--- server.js
+++ server.js
@@ -37,5 +37,5 @@ const server = app.listen(8081, function() {
     var host = server.address().address;
     var port = server.address().port;
 
-    console.log(`Example app listening at http://${host}:${ port}`);
+    console.log(`Dorinda is in the house, listening at http://${host}:${ port}`);
 });
